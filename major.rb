fb94d2c749b050a7c9b6a236de7f645b756567ee
Domikado 8 <domikado8@Domikado-8.local>
2013-11-22 15:50:09 +0700

    fix major

--- major.rb
+++ major.rb
@@ -1,6 +1,6 @@
 class Major < ActiveRecord::Base
-  has_many :college_faculty_major
-  has_many :faculty, through: :college_faculty_major
+  has_many :college_faculty_majors
+  has_many :faculties, through: :college_faculty_major
 
   has_many :college_faculty_major_courses
   has_many :semesters, through: :college_faculty_major_courses
