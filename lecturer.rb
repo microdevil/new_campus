5fd9af9ff9c3aa28c66f6055a9cad37de7b56946
Domikado 2 <domikado2@Domikado-2.local>
2013-11-22 14:39:16 +0700

    lecturers has_many courses and has_many colleges

--- lecturer.rb
+++ lecturer.rb
@@ -1,3 +1,7 @@
 class Lecturer < ActiveRecord::Base
+  has_many :lecturer_course
+  has_many :courses, through: :lecturer_course
 
+  has_many :college_lecturer
+  has_many :colleges, through: :college_lecturer
 end
