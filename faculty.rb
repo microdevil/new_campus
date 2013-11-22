87286686266614a2cf5f6113265c311a3f44b799
Domikado 9 <domikado9@Domikado-9.local>
2013-11-22 14:56:50 +0700

    faculty.rb

---
+++ faculty.rb
@@ -0,0 +1,6 @@
+class faculty
+  has_many :college_faculty_majors
+  has_many :majors through: :college_faculty_majors
+  has_many :college_faculties
+  has_many :colleges through: :college_faculties
+end
