7e226d6b34c531f19b4d72b71069cb92fe825074
Domikado 3 <domikado3@Domikado-3.local>
2013-11-22 15:00:36 +0700

    course,college_faculty_major new

--- course.rb
+++ course.rb
@@ -6,10 +6,10 @@ class Course < ActiveRecord::Base
   has_many :semesters, through: :college_faculty_major_courses
 
   has_many :college_faculty_major_courses
-  has_many :college_faculty_majors through: :college_faculty_major_courses
+  has_many :college_faculty_majors, through: :college_faculty_major_courses
 
   has_many :college_faculty_major_courses
-  has_many :college_faculty_majors through: :college_faculty_major_courses
+  has_many :college_faculty_majors, through: :college_faculty_major_courses
 end
 
 
