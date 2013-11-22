8d72fd7fa9ac0458407105af975c4a784a95b634
Domikado 9 <domikado9@Domikado-9.local>
2013-11-22 15:59:25 +0700

    Faculty Fix

--- faculty.rb
+++ faculty.rb
@@ -1,6 +1,6 @@
 class Faculty < ActiveRecord::Base
   has_many :college_faculty_majors
-  has_many :majors through: :college_faculty_majors
+  has_many :majors, through: :college_faculty_majors
   has_many :college_faculties
-  has_many :colleges through: :college_faculties
+  has_many :colleges, through: :college_faculties
 end
