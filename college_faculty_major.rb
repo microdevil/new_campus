60566f2e924df54666e3ff66e51960b4f4c1f70f
Domikado 9 <domikado9@Domikado-9.local>
2013-11-22 17:15:53 +0700

    new_name

--- college_faculty_major.rb
+++ college_faculty_major.rb
@@ -3,4 +3,5 @@ class CollegeFacultyMajor < ActiveRecord::Base
   has_many   :courses, through: :college_faculty_major_courses
   belongs_to :college_faculty 
   belongs_to :major
+  has_one    :faculty, through: :college_faculty
 end
